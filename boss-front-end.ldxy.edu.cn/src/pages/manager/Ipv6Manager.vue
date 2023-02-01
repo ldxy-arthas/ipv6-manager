69a4e427c25b1c5d0ab61faabb9b28feef6b5e1e
yuluo <1481556636@qq.com>
2023-02-01 20:04:46 +0800

    update frontend

--- boss-front-end.ldxy.edu.cn/src/pages/manager/Ipv6Manager.vue
+++ boss-front-end.ldxy.edu.cn/src/pages/manager/Ipv6Manager.vue
@@ -1,12 +1,122 @@
 <template>
-    <div>
-        manager
+  <el-card shadow="hover">
+    <el-table :data="ipv6Data" stripe style="width: 100%">
+      <el-table-column prop="ipv6" label="ipv6" width="340" align="center">
+        <template #="scope">
+          <el-tooltip
+            effect="light"
+            content="点击地址复制"
+            placement="top-start"
+          >
+            <span @click="linkUrl(scope.row.ipv6)">{{ scope.row.ipv6 }}</span>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+      <el-table-column prop="region" label="地区" width="180" align="center" />
+      <el-table-column prop="status" label="状态" align="center">
+        <template #="scope">
+          <el-tag effect="dark" v-if="scope.row.status == 1"> 使用中 </el-tag>
+          <el-tag effect="light" v-else> 未使用 </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center">
+        <template #="scope">
+          <div>
+            <el-popover
+              placement="left"
+              :width="275"
+              trigger="click"
+              :content="'分配mac地址为：' + scope.row.mac"
+            >
+              <template #reference>
+                <el-button type="primary">查看</el-button>
+              </template>
+            </el-popover>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="pagination">
+      <el-pagination
+        background
+        pager-count="6"
+        :page-size="9"
+        v-model:currentPage="currentPage"
+        layout="prev, pager, next"
+        :current-page="currentPage"
+        :total="dataTotal"
+        @current-change="handleCurrentChange"
+      />
     </div>
+  </el-card>
 </template>
-<script lang="ts" setup>
+<script setup>
+import { ElMessage } from "element-plus";
+import { ref } from "vue";
 
+// 分页数据
+let currentPage = ref(1);
+let pageSize = ref(9);
+let dataTotal = ref(0);
+
+/**
+ * 在这需要做的是，将当前切换页的页码拿到，去后台查询数据。将数据渲染到前端。
+ */
+const handleCurrentChange = (val) => {
+  let curpage = `${val}`;
+  // console.log(curpage);
+  getData(curpage);
+};
+
+const ipv6Data = [
+  {
+    id: "100001",
+    ipv6: "AD80:0000:0000:0000:ABAA:0000:00C2:0002",
+    region: "庆阳市",
+    status: "1",
+    mac: "00-1A-2B-3C-4D-56",
+  },
+  {
+    id: "100002",
+    ipv6: "fe80::3cda:3a6:dd69:fec9%19",
+    region: "兰州市",
+    status: "0",
+    mac: "",
+  },
+  {
+    id: "100002",
+    ipv6: "AD80:0000:0000:0000:ABAA:0000:00C2:0002",
+    region: "兰州市",
+    status: "0",
+    mac: "",
+  },
+  {
+    id: "100002",
+    ipv6: "AD80:0000:0000:0000:ABAA:0000:00C2:0002",
+    region: "兰州市",
+    status: "1",
+    mac: "",
+  },
+];
+
+const linkUrl = (item) => {
+  let url = item; //拿到想要复制的值
+  let copyInput = document.createElement("input"); //创建input元素
+  document.body.appendChild(copyInput); //向页面底部追加输入框
+  copyInput.setAttribute("value", url); //添加属性，将url赋值给input元素的value属性
+  copyInput.select(); //选择input元素
+  document.execCommand("Copy"); //执行复制命令
+  ElMessage.success("ipv6地址已复制！"); //弹出提示信息，不同组件可能存在写法不同
+  //复制之后再删除元素，否则无法成功赋值
+  copyInput.remove(); //删除动态创建的节点
+};
 </script>
 
-<style lang="scss">
-    
+<style>
+.pagination {
+  margin-top: 2vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
--- boss-front-end.ldxy.edu.cn/src/pages/manager/Ipv6Operation.vue
+++ boss-front-end.ldxy.edu.cn/src/pages/manager/Ipv6Operation.vue
@@ -1,11 +1,93 @@
 <template>
-  <div>
-    ipv6 操作 <br />
-    分配ipv6 随机分配
+  <div class="index">
+    <el-card class="card" shadow="hover">
+      <div>
+        <h3 class="title">分配ipv6</h3>
+        <el-card shadow="always">
+          <el-input
+            placeholder="输入设备mac地址"
+            v-model="inputMac"
+            clearable
+            size="large"
+            class="w-50 m-2"
+          />
+          <br />
+          <el-input
+            placeholder="输入ipv6地址"
+            v-model="inputIpv6"
+            clearable
+            class="w-50 m-2"
+            size="large"
+          />
+          <br />
+          <div class="btn">
+            <el-button type="primary" @click="set">分配</el-button>
+          </div>
+        </el-card>
+      </div>
+    </el-card>
+    <el-card class="card" shadow="hover"
+      ><div>
+        <h3 class="title">随机分配</h3>
+        <el-card shadow="always">
+          <el-input
+            placeholder="输入设备mac地址"
+            v-model="inputMac"
+            clearable
+            size="large"
+            class="w-50 m-2"
+          />
+          <br />
+          <div class="btn">
+            <el-button type="primary" @click="set">分配</el-button>
+          </div>
+        </el-card>
+      </div></el-card
+    >
   </div>
 </template>
 <script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import { ElNotification } from "element-plus";
+
+onMounted(() => {
+  open();
+});
+
+const inputMac = ref("");
+const inputIpv6 = ref("");
+
+const open = () => {
+  ElNotification({
+    title: "Info",
+    message:
+      "分配ipv6时确保填入正确的ipv6地址和设备mac地址，随机分配不需要填写ipv6地址，由dhcp服务器自行分配！",
+    type: "info",
+  });
+};
 </script>
 
-<style lang="scss">
+<style>
+.index {
+  display: flex;
+  justify-content: space-around;
+}
+
+.title {
+  text-align: center;
+  margin-bottom: 2vh;
+  font-weight: 600;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+.card {
+  border-radius: 10%;
+  background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+}
+
+.btn {
+  margin-top: 2vh;
+  text-align: center;
+}
 </style>
